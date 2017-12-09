a4749832bac4b74877bc0ed4154200649911b0ec
JinTak <jtak23@gmail.com>
2017-12-08 19:40:42 -0700

    winning logic added

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,12 @@ var ufo = document.getElementById('ufo');
 var spaceship = document.getElementById('spaceship');
 
 let ufoPosX = 0; 
-let ufoPosY = 0;
+let ufoCounter = 0;
+// let ufoPosY = 0;
 
 let spaceshipPosX = 0; 
-let spaceshipPosY = 0;
+let spaceshipCounter = 0;
+// let spaceshipPosY = 0;
 
 // ufo.addEventListener('keyup', function(e){
 //     console.log(e);
@@ -18,60 +20,81 @@ document.addEventListener('keyup', function(e){
     // console.log(e.keyCode);
 
     // UFO MOVING 
-    if(e.keyCode === 83){
-        // Move Down
-        ufoPosY = ufoPosY + 5;
-        ufo.style.top = ufoPosY + "px";
-        // console.log(posY);
-    } else if(e.keyCode === 87){
-        // console.log("W");
-        // Move Up
-        ufoPosY = ufoPosY -  5;
-        ufo.style.top = ufoPosY + "px";
-        // console.log(posY);
-    } else if(e.keyCode === 65){
-        // console.log("A");
-        // Move Left
-        ufoPosX = ufoPosX - 5;
-        // console.log(ufoPosX);
-        ufo.style.left = ufoPosX + "px";
-        // console.log(posY);
-    } else if(e.keyCode === 68){
+    // if(e.keyCode === 83){
+    //     // Move Down
+    //     ufoPosY = ufoPosY + 3;
+    //     ufo.style.top = ufoPosY + "vh";
+    //     // console.log(posY);
+    // } else if(e.keyCode === 87){
+    //     // console.log("W");
+    //     // Move Up
+    //     ufoPosY = ufoPosY - 3;
+    //     ufo.style.top = ufoPosY + "vh";
+    //     // console.log(posY);
+    // } else 
+    // if(e.keyCode === 65){
+    //     // console.log("A");
+    //     // Move Left
+    //     ufoPosX = ufoPosX - 1;
+    //     // console.log(ufoPosX);
+    //     ufo.style.left = ufoPosX + "vw";
+    //     // console.log(posY);
+    // } else 
+    
+    if(e.keyCode === 68){
         // console.log("D");
         // Move Right
-        ufoPosX = ufoPosX +  5;
+        ufoCounter++;
+        ufoPosX = ufoPosX +  1;
         // console.log(ufoPosX);
-        ufo.style.left = ufoPosX + "px";
-        // console.log(posY);
+        ufo.style.left = ufoPosX + "vw";
+
+        if(ufoCounter == 40){
+            var x = document.getElementById('winText');
+            x.classList.add("won");
+            x.innerHTML = "UFO WINS!";
+        }
     }
 
 
     // SPACESHIP MOVING
-    if(e.keyCode === 75){
-        // console.log("S");
-        // Move Down
-        spaceshipPosY = spaceshipPosY + 5;
-        spaceship.style.top = spaceshipPosY + "px";
-        // console.log(posY);
-    } else if(e.keyCode === 73){
-        // console.log("W");
-        // Move Up
-        spaceshipPosY = spaceshipPosY - 5;
-        spaceship.style.top = spaceshipPosY + "px";
-        // console.log(posY);
-    } else if(e.keyCode === 74){
+    // if(e.keyCode === 75){
+    //     // console.log("S");
+    //     // Move Down
+    //     spaceshipPosY = spaceshipPosY + 3;
+    //     spaceship.style.top = spaceshipPosY + "vh";
+    //     // console.log(posY);
+    // } else if(e.keyCode === 73){
+    //     // console.log("W");
+    //     // Move Up
+    //     spaceshipPosY = spaceshipPosY - 3;
+    //     spaceship.style.top = spaceshipPosY + "vh";
+    //     // console.log(posY);
+    // } else 
+    
+    if(e.keyCode === 74){
         // console.log("A");
         // Move Left
-        spaceshipPosX = spaceshipPosX - 5;
-        spaceship.style.left = spaceshipPosX + "px";
+        spaceshipCounter++;
+        spaceshipPosX = spaceshipPosX + 1;
+        spaceship.style.right = spaceshipPosX + "vw";
         // console.log(posY);
-    } else if(e.keyCode === 76){
-        // console.log("D");
-        // Move Right
-        spaceshipPosX = spaceshipPosX + 5;
-        spaceship.style.left = spaceshipPosX + "px";
-        // console.log(posY);
-    }
+
+        if(spaceshipCounter == 40){
+            var x = document.getElementById('winText');
+            x.classList.add("won");
+            x.innerHTML = "SPACESHIP WINS!";
+        }
+    } 
+    
+    // else if(e.keyCode === 76){
+    //     // console.log("D");
+    //     // Move Right
+        
+    //     spaceshipPosX = spaceshipPosX - 1;
+    //     spaceship.style.right = spaceshipPosX + "vw";
+        
+    // }
 });
 
 
