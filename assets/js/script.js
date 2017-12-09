e4b55a5f3067a07e435e9d6d4ecf08888d776f88
JinTak <jtak23@gmail.com>
2017-12-09 08:20:57 -0700

    added more mobility to game pieces

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,12 @@ var ufo = document.getElementById('ufo');
 var spaceship = document.getElementById('spaceship');
 
 let ufoPosX = 0; 
+let ufoPosY = 50;
 let ufoCounter = 0;
-// let ufoPosY = 0;
 
 let spaceshipPosX = 0; 
+let spaceshipPosY = 45;
 let spaceshipCounter = 0;
-// let spaceshipPosY = 0;
 
 // ufo.addEventListener('keyup', function(e){
 //     console.log(e);
@@ -20,81 +20,89 @@ document.addEventListener('keyup', function(e){
     // console.log(e.keyCode);
 
     // UFO MOVING 
-    // if(e.keyCode === 83){
-    //     // Move Down
-    //     ufoPosY = ufoPosY + 3;
-    //     ufo.style.top = ufoPosY + "vh";
-    //     // console.log(posY);
-    // } else if(e.keyCode === 87){
-    //     // console.log("W");
-    //     // Move Up
-    //     ufoPosY = ufoPosY - 3;
-    //     ufo.style.top = ufoPosY + "vh";
-    //     // console.log(posY);
-    // } else 
-    // if(e.keyCode === 65){
-    //     // console.log("A");
-    //     // Move Left
-    //     ufoPosX = ufoPosX - 1;
-    //     // console.log(ufoPosX);
-    //     ufo.style.left = ufoPosX + "vw";
-    //     // console.log(posY);
-    // } else 
-    
-    if(e.keyCode === 68){
+    if(e.keyCode === 83){
+        // Move Down
+        ufoPosY++;
+        ufo.style.top = ufoPosY + "vh";
+    } else if(e.keyCode === 87){
+        // console.log("W");
+        // Move Up
+        ufoPosY--;
+        ufo.style.top = ufoPosY + "vh";
+        // console.log(posY);
+    } else if(e.keyCode === 65){
+        // console.log("A");
+        // Move Left
+        ufoPosX--;
+        // console.log(ufoPosX);
+        ufo.style.left = ufoPosX + "vw";
+        // console.log(posY);
+    } else if(e.keyCode === 68){
         // console.log("D");
         // Move Right
         ufoCounter++;
-        ufoPosX = ufoPosX +  1;
+        ufoPosX++;
         // console.log(ufoPosX);
         ufo.style.left = ufoPosX + "vw";
 
         if(ufoCounter == 40){
+            removeBitcoin();
             var x = document.getElementById('winText');
             x.classList.add("won");
             x.innerHTML = "UFO WINS!";
+            
         }
     }
 
 
     // SPACESHIP MOVING
-    // if(e.keyCode === 75){
-    //     // console.log("S");
-    //     // Move Down
-    //     spaceshipPosY = spaceshipPosY + 3;
-    //     spaceship.style.top = spaceshipPosY + "vh";
-    //     // console.log(posY);
-    // } else if(e.keyCode === 73){
-    //     // console.log("W");
-    //     // Move Up
-    //     spaceshipPosY = spaceshipPosY - 3;
-    //     spaceship.style.top = spaceshipPosY + "vh";
-    //     // console.log(posY);
-    // } else 
-    
-    if(e.keyCode === 74){
+    if(e.keyCode === 75){
+        // console.log("S");
+        // Move Down
+        spaceshipPosY++
+        spaceship.style.top = spaceshipPosY + "vh";
+        // console.log(posY);
+    } else if(e.keyCode === 73){
+        // console.log("W");
+        // Move Up
+        spaceshipPosY--;
+        spaceship.style.top = spaceshipPosY + "vh";
+        
+        // console.log(posY);
+    }  else if(e.keyCode === 76){
+        // console.log("D");
+        // Move Right
+        
+        spaceshipPosX--;
+        spaceship.style.right = spaceshipPosX + "vw";
+    } else if(e.keyCode === 74){
         // console.log("A");
         // Move Left
         spaceshipCounter++;
-        spaceshipPosX = spaceshipPosX + 1;
+        spaceshipPosX++;
         spaceship.style.right = spaceshipPosX + "vw";
         // console.log(posY);
 
         if(spaceshipCounter == 40){
+            removeBitcoin();
             var x = document.getElementById('winText');
             x.classList.add("won");
             x.innerHTML = "SPACESHIP WINS!";
         }
-    } 
     
-    // else if(e.keyCode === 76){
-    //     // console.log("D");
-    //     // Move Right
         
-    //     spaceshipPosX = spaceshipPosX - 1;
-    //     spaceship.style.right = spaceshipPosX + "vw";
-        
+    }
+
+    // function moveUp() { 
+    //     spaceshipPosY++;
+    //     spaceship.style.top = spaceshipPosY + "vh";
     // }
+
+
+    function removeBitcoin(){
+        var bitcoin = document.getElementById('bitcoin');
+        bitcoin.remove();
+    }
 });
 
 
